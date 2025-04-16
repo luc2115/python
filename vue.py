66fc2e094915ee6685664a51fafa14fbfe00f93e
luc2115 <luc.bourgeoisj@gmail.com>
2025-04-16 16:40:48 +0200

    remplacement # par """

--- vue.py
+++ vue.py
@@ -1,23 +1,30 @@
 class Affichage():
   
   def affichage_text():
-  #recupère la liste des mots a afficher et affiche les premiers mots de la liste dans la zone d'affichage
-  #losque la touche espace est appuyée le premier mot de la liste disparait et le reste de la liste se décale vers la gauche 
-  # + la suite de la liste (mot suivant dans la liste) apparait a la fin de la ligne
+      """
+      recupère la liste des mots a afficher et affiche les premiers mots de la liste dans la zone d'affichage
+      losque la touche espace est appuyée le premier mot de la liste disparait et le reste de la liste se décale vers la gauche 
+      + la suite de la liste (mot suivant dans la liste) apparait a la fin de la ligne
+      """
 
   def couleur_background():
-    #change la couleur du fond en fonction de la couleur reçue par la class interface
-    
+      """"
+      change la couleur du fond en fonction de la couleur reçue par la class interface
+      """
+      
   def nombre_erreurs():
-    #si la touche backspace est appuyée on ajoute 1 a la variable nb_erreurs
-    #(cette variable sera envoyée pur traitement et utilisée dans la page résultats)
+      """
+      si la touche backspace est appuyée on ajoute 1 a la variable nb_erreurs
+      (cette variable sera envoyée pur traitement et utilisée dans la page résultats)
+      """
     
   def text_utilisateur():
-    #event= appui sur la touche espace
-    #recupère le text écrit par l'utilisateur dans la zone de texte (variable text=".....")
-    #réinitialise la zone de text de l'interface pour que l'utilisateur puisse ecrire un nouveau mot
-    #renvoie la variable text
-
+      """
+      event= appui sur la touche espace
+      recupère le text écrit par l'utilisateur dans la zone de texte (variable text=".....")
+      réinitialise la zone de text de l'interface pour que l'utilisateur puisse ecrire un nouveau mot
+      renvoie la variable text
+      """
   def demarrer_le_jeu():
         """
         Lorsque une lettre est pressée dans la zone de texte,
