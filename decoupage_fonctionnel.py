e928bb6aa38ff4e1e6947f8edf53de9d8aa914eb
luc2115 <luc.bourgeoisj@gmail.com>
2025-04-16 14:51:15 +0200

    debut methode class interface

--- decoupage_fonctionnel.py
+++ decoupage_fonctionnel.py
@@ -20,6 +20,38 @@ class Calcul():
 
 
 class Interface():
+    def __init__():
+        """
+        
+        """
+    
+    def recuperation_reponse_joueur():
+        """
+        recupere la reponse de l'utilisateur depuis l'interface
+
+        """
+        
+    def verification():
+        """
+        verifie que la reponse correspond au mot affiché
+        """
+        
+    def backspace():
+        """
+        renvoie un booléen si la touche backspace est utilisé
+
+        """
+    
+    def type_de_jeu_choisi():
+        """
+        recupère le type de jeu choisi par le joueur
+        """
+    
+    def creation_liste_mots ():
+        """
+        renvoie en fonction du type de jeu choisi, une liste de mots pour la partie à partir de la banque de données
+
+        """
 
 
 
