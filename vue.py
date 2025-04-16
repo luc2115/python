8c3db77568bb1c3473bc74acf2df635b1acf51d7
luc2115 <luc.bourgeoisj@gmail.com>
2025-04-16 16:38:16 +0200

    tabulation def

--- vue.py
+++ vue.py
@@ -1,18 +1,18 @@
 class Affichage():
   
-def affichage_text():
-#recupère la liste des mots a afficher et affiche les premiers mots de la liste dans la zone d'affichage
-#losque la touche espace est appuyée le premier mot de la liste disparait et le reste de la liste se décale vers la gauche 
-# + la suite de la liste (mot suivant dans la liste) apparait a la fin de la ligne
+  def affichage_text():
+  #recupère la liste des mots a afficher et affiche les premiers mots de la liste dans la zone d'affichage
+  #losque la touche espace est appuyée le premier mot de la liste disparait et le reste de la liste se décale vers la gauche 
+  # + la suite de la liste (mot suivant dans la liste) apparait a la fin de la ligne
 
-def couleur_background():
+  def couleur_background():
     #change la couleur du fond en fonction de la couleur reçue par la class interface
     
-def nombre_erreurs():
+  def nombre_erreurs():
     #si la touche backspace est appuyée on ajoute 1 a la variable nb_erreurs
     #(cette variable sera envoyée pur traitement et utilisée dans la page résultats)
     
-def text_utilisateur():
+  def text_utilisateur():
     #event= appui sur la touche espace
     #recupère le text écrit par l'utilisateur dans la zone de texte (variable text=".....")
     #réinitialise la zone de text de l'interface pour que l'utilisateur puisse ecrire un nouveau mot
@@ -28,7 +28,7 @@ def text_utilisateur():
         
         """
         
-    def arreter_le_jeu():
+  def arreter_le_jeu():
         """
         si mode_de_jeu = course_contre_la_montre:
             le jeu s'arrête quand le compte à rebourd arrive à 0
@@ -38,14 +38,14 @@ def text_utilisateur():
         les résultats et statistiques du joueur sur cette partie.
         
         """
-    def reinitialiser():
+  def reinitialiser():
         """
         fonction lier au bouton 'RESTART' qui reinitialise toutes les variables quand le bouton est pressé,
         par exemple si le joueur a fait un mauvais départ ou veux faire une nouvelle partie
         
         """
         
-    def chrono():
+  def chrono():
         """
         affiche un compte à rebours qui se déclenche avec la fonction 'demarrer_le_jeu'
         
