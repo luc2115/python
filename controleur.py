8762b1583e7df1d92172ad0bd583493527a3387d
luc2115 <luc.bourgeoisj@gmail.com>
2025-04-16 15:35:34 +0200

    Create controleur.py + ajout classe Interface

---
+++ controleur.py
@@ -0,0 +1,48 @@
+class Interface():
+    def __init__():
+        """
+        
+        """
+    def recup_mot():
+        """
+        recupere le mot tapé par l'utilisateur
+        """
+        
+    def verification():
+        """
+        verifie que la reponse correspond au mot affiché et renvoie un booléen
+        """
+        
+    def compte_lettre():
+        """
+        compte le nombre de caractere du mot tapé qu'il soit juste ou non
+        
+        """
+        
+    def backspace():
+        """
+        renvoie un booléen si la touche backspace est utilisé
+
+        """
+    
+    def type_de_jeu_choisi():
+        """
+        recupère le type de jeu choisi par le joueur
+        """
+    
+    def creation_liste_mots():
+        """
+        renvoie en fonction du type de jeu choisi, une liste de mots pour la partie à partir de la banque de données
+
+        """
+    
+    def recupere_stats():
+        """
+        recupere les stats du joueur pendant la partie pour les envoyer à l'affichage
+
+        """
+    
+    def recupere_streak():
+        """
+        recupere le score de combo et y associe une couleur pour l'affichage 
+        """
