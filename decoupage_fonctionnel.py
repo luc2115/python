47f92d811bdac31286629074d4abfad468cf224c
luc2115 <luc.bourgeoisj@gmail.com>
2025-04-16 15:13:00 +0200

    modif methode classe interface

--- decoupage_fonctionnel.py
+++ decoupage_fonctionnel.py
@@ -24,16 +24,20 @@ class Interface():
         """
         
         """
-    
-    def recuperation_reponse_joueur():
+    def recup_mot():
         """
-        recupere la reponse de l'utilisateur depuis l'interface
-
+        recupere le mot tapé par l'utilisateur
         """
         
     def verification():
         """
-        verifie que la reponse correspond au mot affiché
+        verifie que la reponse correspond au mot affiché et renvoie un booléen
+        """
+        
+    def compte_lettre():
+        """
+        compte le nombre de caractere du mot tapé qu'il soit juste ou non
+        
         """
         
     def backspace():
@@ -47,11 +51,37 @@ class Interface():
         recupère le type de jeu choisi par le joueur
         """
     
-    def creation_liste_mots ():
+    def creation_liste_mots():
         """
         renvoie en fonction du type de jeu choisi, une liste de mots pour la partie à partir de la banque de données
 
         """
+    
+    def recupere_stats():
+        """
+        recupere les stats du joueur pendant la partie pour les envoyer à l'affichage
+
+        """
+    
+    def recupere_streak():
+        """
+        recupere le score de combo et y associe une couleur pour l'affichage 
+        """
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 
