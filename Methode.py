18bb9eca7bbf4e75f6a14b31678746580a36cd2a
luc2115 <luc.bourgeoisj@gmail.com>
2025-04-16 15:34:54 +0200

    ajout classe Score

---
+++ Methode.py
@@ -0,0 +1,52 @@
+class Score():
+    
+    def __init__(self,):
+         """
+         on verra
+         """
+    def nb_caracteres_mot_courant(self):
+        """
+        calcule le nb de caracteres d'un mot'
+        """
+    def compte_caracteres(self):
+        """
+        Un compteur qui fait +1 pour chaque caractère des mots
+
+        """
+    def compte_back_space(self):
+        """
+        un compteur qui fait +1 dès que le joueur fait un backspace
+        """
+    def streak(self):
+        """
+        Combien de bons mots sans avoir fait d'erreurs (un backspace remet le compteur à 0)
+        """
+    def bonus(self):
+        """
+        Prend en entrée la streak et donne des points en plus en fonction 
+        Streak à 5 mots, +10% points
+        Streak à 10 mots, +20% points
+        Le 20ème mot bien écrit compte double puis on reste à +25% après
+        """
+    def point_du_mot(self):
+        """
+        Utilise bonus, le nb_de_caractères_du_mot
+        et associe, si le mot est
+        """
+    def compteur_de_points(self):
+        """
+        Compte les points !!
+        """
+    def calcul_precision(self):
+        """
+        100 - compte_back_space/Nombre de caractères
+        """
+    def mots_reussis(self):
+        """
+        le nom parle de lui meme
+        """
+    def mots_rates(self):
+        """
+        le nom parle de lui meme
+        """
+
