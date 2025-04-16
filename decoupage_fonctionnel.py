7088e6391f2b7d740e5bf5be0015ed72ad4ac241
luc2115 <luc.bourgeoisj@gmail.com>
2025-04-16 14:33:10 +0200

    debut ajout fonction class banque

--- decoupage_fonctionnel.py
+++ decoupage_fonctionnel.py
@@ -1,14 +1,25 @@
-Class Affichage():
+class Affichage():
 
 
-Class Banque():
+class Banque():
+    def __init__(self,fichier_csv):
+        """
+        fais appel à la fonction lecture 
+        """
+        
+    def transforme_into_csv (self):
+        """
+        transforme le fichier csv en dictionnaire de mot
+        """
 
 
-Class Calcul():
+
+
+class Calcul():
 
 
 
-Class Interface():
+class Interface():
 
 
 
