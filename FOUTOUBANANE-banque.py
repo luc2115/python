315a9a6a6378412cff3ec0feb5373914e1f82bcd
OreillePluie <71928457+OreillePluie@users.noreply.github.com>
2025-05-07 14:58:12 +0200

    Create FOUTOUBANANE-banque.py

---
+++ FOUTOUBANANE-banque.py
@@ -0,0 +1,17 @@
+import csv
+import random
+
+class Banque:
+    def __init__(self, fichier_csv):
+        self.fichier_csv = fichier_csv
+        self.liste_mots = self.transforme_into_list()
+
+    def transforme_into_list(self):
+        """Transforme le fichier CSV en liste de mots"""
+        mots = []
+        with open(self.fichier_csv, newline='', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                mots.extend(row)
+        random.shuffle(mots)
+        return mots
