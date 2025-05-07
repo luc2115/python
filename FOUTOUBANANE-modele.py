a645bf0f15b34f1a6ed590f1f06c09a9fe0a9696
OreillePluie <71928457+OreillePluie@users.noreply.github.com>
2025-05-07 14:56:11 +0200

    Create FOUTOUBANANE-modele.py

---
+++ FOUTOUBANANE-modele.py
@@ -0,0 +1,41 @@
+
+class Score:
+    def __init__(self):
+        self.points = 0
+        self.caracteres_total = 0
+        self.backspaces = 0
+        self.streak = 0
+        self.mots_reussis = 0
+        self.mots_rates = 0
+
+    def compte_caractere(self, mot):
+        self.caracteres_total += len(mot)
+
+    def compte_backspace(self):
+        self.backspaces += 1
+        self.streak = 0
+
+    def mot_reussi(self, mot):
+        self.streak += 1
+        self.mots_reussis += 1
+        bonus = self.calcul_bonus()
+        self.points += len(mot) * bonus
+        self.compte_caractere(mot)
+
+    def mot_rate(self, mot):
+        self.streak = 0
+        self.mots_rates += 1
+
+    def calcul_bonus(self):
+        if self.streak >= 3:
+            return 2.0
+        elif self.streak >= 2:
+            return 1.25
+        elif self.streak >= 1:
+            return 1.10
+        return 1.0
+
+    def precision(self):
+        if self.caracteres_total == 0:
+            return 100.0
+        return 100 - (self.backspaces / self.caracteres_total * 100)
